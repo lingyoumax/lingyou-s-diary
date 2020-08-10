71dc12eef898b483289a68f3cbdd89b9bb5707e8
陶叶 <54953768+lingyoumax@users.noreply.github.com>
2020-08-10 13:07:59 +0800

    Update P1026.cpp

--- P1026.cpp
+++ P1026.cpp
@@ -6,7 +6,15 @@ string str = "";
 int p, k, s;
 string word[6];
 int w[202][202];
-int f[202][42];
+int f[202][2];
+int check(string st) {
+	for (int i = 0;i < s;i++) {
+		if (st.find(word[i]) == 0) {
+			return 1;
+		}
+	}
+	return 0;
+}
 int main() {
 	cin >> p >> k;
 	for (int i = 0;i < p;i++) {
@@ -24,28 +32,26 @@ int main() {
 			w[j][i] = w[j + 1][i];
 			bool flag = false;
 			string st = str.substr(j - 1, i - j + 1);
-			for (int k = 0;k < s;k++) {
-				string::size_type position;
-				position = st.find(word[k]);
-				if (position==0) {
-					flag = true;
-				}
-			}
-			if (flag) {
-				w[j][i]++;
-			}
+			w[j][i] += check(st);
 		}
 	}
+	bool b = 0;
 	for (int i = 1;i <= len;i++) {
-		f[i][1] = w[1][i];
+		f[i][b] = w[1][i];
 	}
-	for (int j = 2;j <= k;j++) {
-		for (int i = j;i <= len;i++) {
-			for (int m = (j - 1);m <= (i - 1);m++) {
-				f[i][j] = max(f[i][j], f[m][j - 1] + w[m + 1][i]);
+	for (int cs = 2;cs < k;cs++) {
+		for (int i = cs;i <= len;i++) {
+			f[i][!b] = 0;
+			for (int j = cs - 1;j < i;j++) {
+				f[i][!b] = max(f[i][!b], f[j][b] + w[j + 1][i]);
 			}
 		}
+		b = !b;
 	}
-	cout << f[len][k] << endl;
+	int ans = 0;
+	for (int i = (k - 1);i < len;i++) {
+		ans = max(ans, f[i][b] + w[i + 1][len]);
+	}
+	cout << ans << endl;
 	return 0;
 }
